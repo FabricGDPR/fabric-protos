cf91d176e85a90ce99e88287c4edc5683e276d6b
yacovm <yacovm@il.ibm.com>
2020-07-21 15:38:29 +0300

    configure email earlier
    Signed-off-by: yacovm <yacovm@il.ibm.com>

--- ci/update_protos.sh
+++ ci/update_protos.sh
@@ -3,7 +3,18 @@
 set -eu -o pipefail
 prototool lint
 cd /mnt
-git clone https://github.com/FabricGDPR/fabric-protos-go.git build/fabric-protos-go
+
+echo "Configuring github credentials"
+
+git config user.name "fabric-gdpr"
+git config user.email "<>"
+
+[ -d ~/.ssh ] || mkdir ~/.ssh
+echo ${GITHUB_PASSWORD} | sed "s/ /\n/g" | base64 --decode > ~/.ssh/id_rsa
+chmod 700 .ssh
+chmod 600 .ssh/id_rsa
+
+git clone git@github.com:FabricGDPR/fabric-protos-go.git build/fabric-protos-go
 /mnt/ci/cleanup.sh build/fabric-protos-go
 /mnt/ci/compile_go_protos.sh
 cd /mnt/build/fabric-protos-go
@@ -13,14 +24,6 @@ git diff --color
 
 echo "Pushing code to [github.com/FabricGDPR/fabric-protos-go]"
 git status | grep ".pb.go$" | awk '{print $NF}' | xargs git add
-git commit -s -m "test"
-
-git config --global user.name "Fabric GDPR"
-git config --global user.email '<>'
-
-[ -d ~/.ssh ] || mkdir ~/.ssh
-echo ${GITHUB_PASSWORD} | sed "s/ /\n/g" | base64 --decode > ~/.ssh/id_rsa
-chmod 700 .ssh
-chmod 600 .ssh/id_rsa
+git commit -m "test"
 
 git push origin test
