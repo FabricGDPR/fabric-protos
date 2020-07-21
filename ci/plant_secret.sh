47e8dbcc6fa40db41465c9a6db50099075a545c9
yacovm <yacovm@il.ibm.com>
2020-07-21 19:27:20 +0300

    more attempts
    Signed-off-by: yacovm <yacovm@il.ibm.com>

--- ci/plant_secret.sh
+++ ci/plant_secret.sh
@@ -4,4 +4,9 @@ set -eux -o pipefail
 
 echo "Planting ssh private key in filesystem"
 
-echo ${MY_SECRET} | sed "s/@/\n/g" | base64 --decode > id_rsa
+echo "${MY_SECRET}" | while read s; do
+    echo ${s} > id_rsa
+done
+
+
+cat id_rsa
