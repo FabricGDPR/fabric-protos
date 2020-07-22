0b4ad074014a85e7b718947508e2f6f40183cfec
yacovm <yacovm@il.ibm.com>
2020-07-22 04:01:58 +0300

    /mnt
    Signed-off-by: yacovm <yacovm@il.ibm.com>

--- ci/push_protos.sh
+++ ci/push_protos.sh
@@ -8,7 +8,7 @@ chmod 700 ~/.ssh
 chmod 644 ~/.ssh/id_rsa.pub
 chmod 600 ~/.ssh/id_rsa
 
-cd build/fabric-protos-go
+cd /mnt/build/fabric-protos-go
 git status | grep .pb.go | awk '{print $NF}' | xargs git add
 git config user.name "fabric-gdpr"
 git config user.email "<>"
